6de976d00db59c2502023aaca340c8e1d4f6c7b9
Yaroslava Hryzadubova <yaroslava.gd@gmail.com>
2023-05-17 00:31:05 +0200

    feat: implement css for cell

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
 import './index.html';
 import './index.scss';
-import bomb from './img/mine-explosion-white.svg';
 import {mult, sum} from './modules/calc';
-
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = bomb;
-img.width = 400;
-
-imgWrap.append(img);
-
-console.log(mult(2,5));
-console.log(sum(2,5));
